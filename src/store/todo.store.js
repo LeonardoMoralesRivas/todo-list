98a80a3752ac35005f7e05c5431229513e2f4a64
Leonardo Morales <leomrivas2004@gmail.com>
2026-02-22 14:19:12 -0600

    feat: Implementacion de funcionalidades principales como: 1. Persistencia de tareas con localStorage 2.Filtros:Completed, Pending and All. 3. Eliminacion de tareas completada.

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,7 +1,7 @@
 import { validate } from "uuid";
 import { Todo } from "../todos/models/todo.model"
 
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'Completed',
     Pending: 'Pending'
@@ -21,15 +21,21 @@ const state = {
 }
 
 const initStore = () =>{
-    console.log(state);
-
+    loadStore();
     console.log('InitStore 🐴');
 }
 
 const loadStore = () =>{
-    throw new Error('Not implemented');
+    if(!localStorage.getItem('state')) return;
+
+    const {todos=[], filter= Filters.All} = JSON.parse(localStorage.getItem('state'));
+    state.todos = todos;
+    state.filter = filter;
 }
 
+const saveStateToLocalStorage = ()=>{
+    localStorage.setItem('state',JSON.stringify(state));
+}
 const getTodos = (filter = Filters.All) =>{
     switch(filter) {
         case Filters.All:   
@@ -50,6 +56,7 @@ const addTodo= (description) =>{
     if (!description) throw new Error('description is required')
     
     state.todos.push( new Todo(description));
+    saveStateToLocalStorage();
 }
 
 const toggleTodo= (todoId)=> {
@@ -59,18 +66,22 @@ const toggleTodo= (todoId)=> {
         }
         return todo; 
     })
+    saveStateToLocalStorage();
 }
 
 const deleteTodo= (todoId)=> {
     state.todos = state.todos.filter(todo => todo.id !== todoId);
+    saveStateToLocalStorage();
 }
 
 const deleteComplete=() =>{
-    state.todos = state.todos.filter(todo => todo.done);
+    state.todos = state.todos.filter(todo => !todo.done);
+    saveStateToLocalStorage();
 }
 
 const setFilter = (newFilter = Filters.All) =>{
     state.filter = newFilter;
+    saveStateToLocalStorage();
 }
 
 const getCurrentFilter = () =>{
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,9 +1,13 @@
-import todoStore from '../store/todo.store';
+import todoStore, { Filters } from '../store/todo.store';
 import html from './app.html?raw'
-import { renderTodos } from './use-cases';
+import { renderPending, renderTodos } from './use-cases';
 
 const ElementsID = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
+    ClearCompleted: '.clear-completed',
+    TodoFilters: '.filtro',
+    PendingCountLabel: '#pending-count',
 
 }
 /**
@@ -16,8 +20,12 @@ export const App = ( elementId ) => {
 
     const displayTodos= () =>{
         const todos = todoStore.getTodos(todoStore.getCurrentFilter());
-        console.log(todos);
         renderTodos(ElementsID.TodoList, todos);
+        updatePendingCount();
+    }
+
+    const updatePendingCount= () =>{
+        renderPending(ElementsID.PendingCountLabel);
     }
 
     //Cuando la funcion App() se llama
@@ -27,4 +35,66 @@ export const App = ( elementId ) => {
         document.querySelector(elementId).append(app);
         displayTodos();
     })();
+
+    //Referencias HTML
+
+    const newDescriptionInput = document.querySelector(ElementsID.NewTodoInput);
+    const todoListUL = document.querySelector(ElementsID.TodoList);
+    const clearCompletedButton = document.querySelector(ElementsID.ClearCompleted);
+    const FiltersLIs = document.querySelectorAll(ElementsID.TodoFilters)
+
+
+    //Listeners
+    newDescriptionInput.addEventListener('keyup', (event)=>{
+        if (event.keyCode !== 13) return;
+        if (event.target.value.trim().length === 0) return;
+
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value = '';
+    });
+
+    todoListUL.addEventListener('click', (event)=>{
+        const element = event.target.closest('[data-id]');
+        todoStore.toggleTodo(element.getAttribute('data-id'));
+        displayTodos();
+    });
+
+    todoListUL.addEventListener('click', (event)=>{
+        const isDestroyElement = (event.target.className === 'destroy');
+        const element = event.target.closest('[data-id]');
+        if (!element || !isDestroyElement) return;
+
+        todoStore.deleteTodo(element.getAttribute(['data-id']));
+        displayTodos();
+        
+    });
+
+
+    clearCompletedButton.addEventListener('click', ()=>{
+        todoStore.deleteComplete();
+        displayTodos();
+    });
+    
+
+    FiltersLIs.forEach(element =>{
+        element.addEventListener('click', (element)=>{
+            FiltersLIs.forEach(el => el.classList.remove('selected'))
+            element.target.classList.add('selected');
+
+            switch(element.target.text){
+                case 'Todos':
+                    todoStore.setFilter(Filters.All)
+                    break;
+                case 'Pendientes':
+                    todoStore.setFilter(Filters.Pending)
+                    break;
+                case 'Completados':
+                    todoStore.setFilter(Filters.Completed)
+                    break;
+                
+            }
+            displayTodos();
+        });
+    })
 }
